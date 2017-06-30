89254f36dc9994ba9b89c76b67e0fc997389c93f
bschenk <bschenk@precognox.com>
2017-06-30 10:39:14 +0200

    init hello world

---
+++ src/main/java/com/schenkbarnabas/tlog16rs/core/Greeting.java
@@ -0,0 +1,22 @@
+package com.schenkbarnabas.tlog16rs.core;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
+public class Greeting {
+
+    @JsonProperty
+    private String greeting;
+
+    public Greeting() {
+    }
+
+    public Greeting(String greeting) {
+        this.greeting = greeting;
+    }
+
+    public String getGreeting() {
+        return greeting;
+    }
+
+}
